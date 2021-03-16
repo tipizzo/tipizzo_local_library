cddd92f011cde74d8fffdb81756bdafb348254e7
david <Davkabanga96@gmail.com>
2021-03-16 14:41:46 +0200

    #'

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
-# Create your views here.
+def index(request):
+    num_books = Book.objects.all().count() # Generate total number of books
+    num_instances = BookInstance.objects.all().count() # Generate total number of BookInstances
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count() #Available books (status = 'a)
+    num_authors = Author.objects.count() # Note that 'all()' function is implied by default
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render the HTML template index.html with the data in context variable
+    return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
